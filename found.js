51c6bb19a0d6be9a68a179616cc2dd95e5769784
Konrad Linkowski <linkowski.konrad97@gmail.com>
2020-08-16 17:34:37 +0200

    3 file type

--- found.js
+++ found.js
@@ -1,8 +1,63 @@
 (() => {
   const $path = document.querySelector('#path')
+  const $folderView = document.querySelector('#folder')
+  const $fileView = document.querySelector('#file')
+  const $imageView = document.querySelector('#image')
+
   const $files = document.querySelector('#files')
+  const fileTypes = ['folder', 'file', 'image']
 
   const hashString = str => str.split('').reduce((hash, char) => (((hash << 5) - hash) + char.charCodeAt(0)) | 0, 0)
+
+  const changeView = view => {
+    $folderView.hidden = view !== 'folder'
+    $fileView.hidden = view !== 'file'
+    $imageView.hidden = view !== 'image'
+  }
+
+  const generateFileName = random => {
+    const length = Math.floor(random() * 15) + 1
+    const start = 'a'.charCodeAt(0)
+    const end = 'z'.charCodeAt(0)
+    const name = Array(length).fill(0).map(() => String.fromCharCode(Math.floor(random() * (end - start)) + start)).join('')
+    return name
+  }
+
+  const getFileType = name => {
+    const hash = Math.abs(hashString(name))
+    const type = fileTypes[hash % 3]
+    return type
+  }
+
+  const generateFileLink = name => {
+    const hash = window.location.hash.slice(1)
+    const parts = [...hash.split('/'), name].filter(e => e)
+    return '#/' + parts.join('/')
+  }
+
+  const createFile = (name, type) => {
+    const $file = document.createElement('li')
+    $file.classList.add('item')
+    const $a = document.createElement('a')
+    $a.classList.add('link', type)
+    $a.textContent = name
+    $file.appendChild($a)
+    $a.href = generateFileLink(name)
+    return $file
+  }
+
+  const renderFiles = files => {
+    while ($files.firstChild) {
+      $files.removeChild($files.firstChild)
+    }
+
+    files.forEach(({ name, type }) => {
+      const $f = createFile(name, type)
+      $files.appendChild($f)
+    })
+
+    changeView('folder')
+  }
 
   const updatePath = () => {
     const hash = window.location.hash.slice(1)
@@ -15,6 +70,12 @@
     }
 
     const parts = hash.split('/')
+
+    if (parts.slice(0, -1).some(part => getFileType(part) !== 'folder')) {
+      window.location.hash = ''
+      return
+    }
+
     parts.forEach((name, i) => {
       const $a = document.createElement('a')
       const href = '#' + parts.slice(0, i + 1).join('/')
@@ -25,48 +86,50 @@
     })
   }
 
-  const generateFileName = random => {
-    const length = Math.floor(random() * 15) + 1
-    const start = 'a'.charCodeAt(0)
-    const end = 'z'.charCodeAt(0)
-    const name = Array(length).fill(0).map(() => String.fromCharCode(Math.floor(random() * (end - start)) + start)).join('')
-    return name
-  }
-
-  const generateFileLink = name => {
-    const hash = window.location.hash.slice(1)
-    const parts = [...hash.split('/'), name].filter(e => e)
-    return '#/' + parts.join('/')
-  }
-
-  const createFile = name => {
-    const $file = document.createElement('li')
-    $file.classList.add('item')
-    const $a = document.createElement('a')
-    $a.classList.add('link', 'folder')
-    $a.textContent = name
-    $a.href = generateFileLink(name)
-    $file.appendChild($a)
-    return $file
-  }
-
-  const renderFiles = files => {
-    while ($files.firstChild) {
-      $files.removeChild($files.firstChild)
-    }
-
-    files.forEach(file => {
-      const $f = createFile(file)
-      $files.appendChild($f)
+  const generateFolder = () => {
+    const fileCount = Math.abs(hashString(window.location.hash)) % 25 + 10
+    const random = new Math.seedrandom(window.location.hash)
+    const files = Array(fileCount).fill(0).map(() => {
+      const name = generateFileName(random)
+      const type = getFileType(name)
+      return { name, type }
     })
+    const folders = files.filter(f => f.type === 'folder')
+    const notFolders = files.filter(f => f.type !== 'folder')
+    
+    const sortedFiles = [
+      ...folders.sort((a, b) => a.name.localeCompare(b.name)),
+      ...notFolders.sort((a, b) => a.name.localeCompare(b.name))
+    ]
+
+    renderFiles(sortedFiles)
+  }
+
+  const generateFile = () => {
+    changeView('file')
+  }
+
+  const generateImage = () => {
+    changeView('image')
   }
 
   const onHashChange = () => {
     updatePath()
-    const fileCount = Math.abs(hashString(window.location.hash)) % 25 + 10
-    const random = new Math.seedrandom(window.location.hash)
-    const files = Array(fileCount).fill(0).map(() => generateFileName(random))
-    renderFiles(files) 
+    const thisFileName = window.location.hash.split('/').slice(-1)[0]
+    if (thisFileName === '') {
+      generateFolder()
+      return
+    }
+
+    const thisFileType = getFileType(thisFileName)
+    
+    const typeMachine = {
+      folder: generateFolder,
+      file: generateFile,
+      image: generateImage
+    }
+
+    typeMachine[thisFileType]()
   }
 
   window.addEventListener('hashchange', onHashChange)
