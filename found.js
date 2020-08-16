dc8d1ae5b50a7b7bd085455ba0752ca786a3f76b
Konrad Linkowski <linkowski.konrad97@gmail.com>
2020-08-16 18:05:55 +0200

    random image and file

--- found.js
+++ found.js
@@ -1,10 +1,13 @@
 (() => {
   const $path = document.querySelector('#path')
-  const $folderView = document.querySelector('#folder')
-  const $fileView = document.querySelector('#file')
-  const $imageView = document.querySelector('#image')
+  const $folderView = document.querySelector('#folder-view')
+  const $fileView = document.querySelector('#file-view')
+  const $imageView = document.querySelector('#image-view')
 
   const $files = document.querySelector('#files')
+  const $image = document.querySelector('#image')
+  const $file = document.querySelector('#file')
+
   const fileTypes = ['folder', 'file', 'image']
 
   const hashString = str => str.split('').reduce((hash, char) => (((hash << 5) - hash) + char.charCodeAt(0)) | 0, 0)
@@ -15,8 +18,8 @@
     $imageView.hidden = view !== 'image'
   }
 
-  const generateFileName = random => {
-    const length = Math.floor(random() * 15) + 1
+  const randomString = (random, minLength = 1, maxLength = 15) => {
+    const length = Math.floor(random() * (maxLength - minLength)) + minLength
     const start = 'a'.charCodeAt(0)
     const end = 'z'.charCodeAt(0)
     const name = Array(length).fill(0).map(() => String.fromCharCode(Math.floor(random() * (end - start)) + start)).join('')
@@ -46,7 +49,7 @@
     return $file
   }
 
-  const renderFiles = files => {
+  const renderFolder = files => {
     while ($files.firstChild) {
       $files.removeChild($files.firstChild)
     }
@@ -55,8 +58,73 @@
       const $f = createFile(name, type)
       $files.appendChild($f)
     })
+  }
 
-    changeView('folder')
+  const generateFolder = () => {
+    const fileCount = Math.abs(hashString(window.location.hash)) % 25 + 10
+    const random = new Math.seedrandom(window.location.hash)
+    const files = Array(fileCount).fill(0).map(() => {
+      const name = randomString(random)
+      const type = getFileType(name)
+      return { name, type }
+    })
+    const folders = files.filter(f => f.type === 'folder')
+    const notFolders = files.filter(f => f.type !== 'folder')
+    
+    const sortedFiles = [
+      ...folders.sort((a, b) => a.name.localeCompare(b.name)),
+      ...notFolders.sort((a, b) => a.name.localeCompare(b.name))
+    ]
+
+    renderFolder(sortedFiles)
+  }
+
+  const renderFile = text => {
+    $file.textContent = text
+  }
+
+  const generateFile = name => {
+    const hash = Math.abs(hashString(name))
+
+    const random = new Math.seedrandom(hash)
+
+    const wordCount = random() * 500 + 100 | 0
+    
+    const words = Array(wordCount).fill(0).map(() => randomString(random, 3, 20))
+
+    const text = words.join(' ')
+
+    renderFile(text)
+  }
+
+  const renderImage = (src, name) => {
+    $image.src = src
+    $image.alt = name
+  }
+
+  const generateImage = name => {
+    const $canvas = document.createElement('canvas')
+    $canvas.width = 500
+    $canvas.height = 500
+    const ctx = $canvas.getContext('2d')
+
+    const hash = Math.abs(hashString(name))
+    const random = new Math.seedrandom(hash)
+
+    const figureCount = random() * 30 + 5 | 0
+
+    for (let i = 0; i < figureCount; i += 1) {
+      const color = `hsl(${random() * 360 | 0}, ${50}%, ${50}%)`
+      ctx.fillStyle = color
+      const x = random() * 500 | 0
+      const y = random() * 500 | 0
+      const w = random() * 50 + 5 | 0
+      const h = random() * 50 + 5 | 0
+  
+      ctx.fillRect(x, y, w, h)
+    }
+
+    renderImage($canvas.toDataURL(), name)
   }
 
   const updatePath = () => {
@@ -86,38 +154,12 @@
     })
   }
 
-  const generateFolder = () => {
-    const fileCount = Math.abs(hashString(window.location.hash)) % 25 + 10
-    const random = new Math.seedrandom(window.location.hash)
-    const files = Array(fileCount).fill(0).map(() => {
-      const name = generateFileName(random)
-      const type = getFileType(name)
-      return { name, type }
-    })
-    const folders = files.filter(f => f.type === 'folder')
-    const notFolders = files.filter(f => f.type !== 'folder')
-    
-    const sortedFiles = [
-      ...folders.sort((a, b) => a.name.localeCompare(b.name)),
-      ...notFolders.sort((a, b) => a.name.localeCompare(b.name))
-    ]
-
-    renderFiles(sortedFiles)
-  }
-
-  const generateFile = () => {
-    changeView('file')
-  }
-
-  const generateImage = () => {
-    changeView('image')
-  }
-
   const onHashChange = () => {
     updatePath()
     const thisFileName = window.location.hash.split('/').slice(-1)[0]
     if (thisFileName === '') {
       generateFolder()
+      changeView('folder')
       return
     }
 
@@ -129,7 +171,8 @@
       image: generateImage
     }
 
-    typeMachine[thisFileType]()
+    typeMachine[thisFileType](thisFileName)
+    changeView(thisFileType)
   }
 
   window.addEventListener('hashchange', onHashChange)
