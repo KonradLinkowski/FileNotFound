f70587d23dd483402b83072136ddbd1ad77ad683
Konrad Linkowski <linkowski.konrad97@gmail.com>
2020-09-08 23:01:06 +0200

    Add a bit more info

--- assets/found.js
+++ assets/found.js
@@ -23,6 +23,7 @@
         When I try to do anything I'm getting the FileNotFound error and everything glitches.
         Your task is to find the file that doesn't exist.
         Glitch effect intensifies when you are getting closer.
+        Shorter directories names seems to be more corrupted ;)
         Look around for easter eggs.
       `
     },
